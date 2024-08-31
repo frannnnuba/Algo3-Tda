f333065b7de04d002e17b4e33360c90ff2da1cb1
frannnnuba <164945866+frannnnuba@users.noreply.github.com>
2024-08-31 01:06:26 -0300

    Add files via upload
    hay que debugear, tira todo cero

--- busyman.cpp
+++ busyman.cpp
@@ -10,6 +10,20 @@ tuple<int,int> infoActividad;
 std::vector<tuple<int,int>> actividades;
 std::vector<tuple<int,int>> MayoresActividades;
 
+bool esActividadValida(vector<tuple<int,int>> lasDelMejor, tuple<int,int> candidata){
+    int inicioCand = get<0>(candidata);
+    int finCand =  get<1>(candidata);
+    for (int j =0; j < lasDelMejor.size();j++){
+        int inicioSeleccionadas = get<0>(lasDelMejor[j]);
+        int finSeleccionadas =  get<1>(lasDelMejor[j]);
+        if (inicioSeleccionadas <= inicioCand <= finSeleccionadas || 
+        inicioSeleccionadas <= finCand <= finSeleccionadas ){
+            return false; 
+        }
+    }
+    return true;
+}
+
 //toma un vector de las tuplas con actividades que no uso aun, el maximo actual y el vector de la solucion actual
 //si para la que estoy por revisar sirve si no sigue.
 //Necesitaria pasar como parametro el "acumulador" de la solucion parcial que va explorando?? para que le sume uno y compare con el max actual o que calcule todo y  despues vea que hace 
@@ -41,25 +55,13 @@ int fRecursiva(int max,int maxActual, vector<tuple<int,int>> lasQueQuedan, vecto
 }
 
 
-bool esActividadValida(vector<tuple<int,int>> lasDelMejor, tuple<int,int> candidata){
-    int inicioCand = get<0>(candidata);
-    int finCand =  get<1>(candidata);
-    for (int j =0; j < lasDelMejor.size();j++){
-        int inicioSeleccionadas = get<0>(lasDelMejor[j]);
-        int finSeleccionadas =  get<1>(lasDelMejor[j]);
-        if (inicioSeleccionadas <= inicioCand <= finSeleccionadas || 
-        inicioSeleccionadas <= finCand <= finSeleccionadas ){
-            return false; 
-        }
-    }
-    return true;
-}
-
 int cont = 0;
 
 int main() {
+    std::cout<< "Cuantas actividades posibles hay?" << std::endl;
     cin >> N;    //N es la cantidad de actividades que tendremos, i la hora de inicio y j la hora de fin
     while (cont < N){
+        std::cout<< "Ingresa el horario de inicio y luego de finalizacion" << std::endl;
         cin >> i >> j;
         infoActividad = {i,j};   //tupla que tiene el inicio y el final de la actividad
         actividades.push_back(infoActividad);  //aca va a haber que cargar las tuplas
@@ -67,9 +69,12 @@ int main() {
     }
     MayoresActividades = {}; //aca va a ir la lista con los horarios de la mayor cantidad posible hasta el momento
     if (N > 0){
-        fRecursiva(0,0,actividades,MayoresActividades);
+       int aDevo = fRecursiva(0,0,actividades,MayoresActividades);
+       std::cout<< "La cantidad maxima de actividades posibles es " << aDevo << std::endl;
+       return aDevo;
     }
     else {
+        std::cout<< " La cantidad es " << N << std::endl; 
         return 0;
     }
 }
